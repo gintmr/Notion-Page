e77d07251f45165ba0b082fa0f626a7aee5b856a
gintmr <148303202+gintmr@users.noreply.github.com>
2025-01-29 00:35:14 +0800

    Update config.js

--- themes/heo/config.js
+++ themes/heo/config.js
@@ -34,11 +34,11 @@ const CONFIG = {
   // 英雄区右侧推荐文章标签, 例如 [推荐] , 最多六篇文章; 若留空白''，则推荐最近更新文章
   HEO_HERO_RECOMMEND_POST_TAG: '推荐',  
   HEO_HERO_RECOMMEND_POST_SORT_BY_UPDATE_TIME: false, // 推荐文章排序，为`true`时将强制按最后修改时间倒序
-  //   HERO_RECOMMEND_COVER: 'https://cdn.pixabay.com/photo/2015/10/30/20/13/sunrise-1014712_1280.jpg', // 英雄区右侧图片  
+  //   HERO_RECOMMEND_COVER: 'https://cdn.pixabay.com/photo/2015/10/30/20/13/sunrise-1014712_1280.jpg', // 英雄区右侧图片    
 
   // 右侧个人资料卡牌欢迎语，点击可自动切换
   HEO_INFOCARD_GREETINGS: [
-    'Hello！I'm···',
+    "Hello！I'm ···",  
 
   ],
 
@@ -89,7 +89,7 @@ const CONFIG = {
       img_1: '/images/heo/20237359d71b45ab77829cee5972e36f8c30073902.webp',
       color_1: '#f29e39',
       title_2: 'CSS3',
-      img_2: '/images/heo/20237c548846044a20dad68a13c0f0e1502f074602.webp',
+      img_2: '/images/heo/20237c548846044a20dad68a13c0f0e1502f074602.webp',  
       color_2: '#2c51db'
     },
     {
