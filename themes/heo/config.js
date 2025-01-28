2e00b4750b5ff7a5bbe45f14c5a6038eea349bcb
gintmr <148303202+gintmr@users.noreply.github.com>
2025-01-29 00:46:05 +0800

    Update config.js

--- themes/heo/config.js
+++ themes/heo/config.js
@@ -20,11 +20,8 @@ const CONFIG = {
   HEO_HERO_TITLE_1: 'Daliy Record',  
   HEO_HERO_TITLE_2: 'Work Record',  
   HEO_HERO_TITLE_3: 'Interests',  
-  HEO_HERO_TITLE_4: '新版上线',
-  HEO_HERO_TITLE_5: 'NotionNext4.0 轻松定制主题',  
-  HEO_HERO_TITLE_LINK: 'https://tangly1024.com',  
   // 英雄区遮罩文字
-  HEO_HERO_COVER_TITLE: '随便逛逛',
+  HEO_HERO_COVER_TITLE: 'Wander Around',  
 
   // 英雄区显示三个置顶分类
   HEO_HERO_CATEGORY_1: { title: 'Daliy Record', url: '/tag/Daliy Record' },    
@@ -32,18 +29,18 @@ const CONFIG = {
   HEO_HERO_CATEGORY_3: { title: 'Interests', url: '/tag/Interests' },  
 
   // 英雄区右侧推荐文章标签, 例如 [推荐] , 最多六篇文章; 若留空白''，则推荐最近更新文章
-  HEO_HERO_RECOMMEND_POST_TAG: '推荐',  
+  HEO_HERO_RECOMMEND_POST_TAG: 'Recommend',    
   HEO_HERO_RECOMMEND_POST_SORT_BY_UPDATE_TIME: false, // 推荐文章排序，为`true`时将强制按最后修改时间倒序
   //   HERO_RECOMMEND_COVER: 'https://cdn.pixabay.com/photo/2015/10/30/20/13/sunrise-1014712_1280.jpg', // 英雄区右侧图片    
 
   // 右侧个人资料卡牌欢迎语，点击可自动切换
   HEO_INFOCARD_GREETINGS: [
-    "Hello！I'm ···",  
+    "Hello！Here's Gintmr, ",    
 
   ],
 
   // 个人资料底部按钮
-  HEO_INFO_CARD_URL1: '/about',
+  HEO_INFO_CARD_URL1: '/about',  
   HEO_INFO_CARD_ICON1: 'fas fa-user',
   HEO_INFO_CARD_URL2: 'https://github.com/tangly1024',
   HEO_INFO_CARD_ICON2: 'fab fa-github',
@@ -78,7 +75,7 @@ const CONFIG = {
     },
     {
       title_1: 'Swift',
-      img_1: '/images/heo/202328bbee0b314297917b327df4a704db5c072402.webp',
+      img_1: '/images/heo/202328bbee0b314297917b327df4a704db5c072402.webp',  
       color_1: '#eb6840',
       title_2: 'Principle',
       img_2: '/images/heo/2023f76570d2770c8e84801f7e107cd911b5073202.webp',
@@ -111,16 +108,16 @@ const CONFIG = {
   ],
 
   HEO_SOCIAL_CARD: true, // 是否显示右侧，点击加入社群按钮
-  HEO_SOCIAL_CARD_TITLE_1: '交流频道',
+  HEO_SOCIAL_CARD_TITLE_1: '交流频道',  
   HEO_SOCIAL_CARD_TITLE_2: '加入我们的社群讨论分享',
   HEO_SOCIAL_CARD_TITLE_3: '点击加入社群',
   HEO_SOCIAL_CARD_URL: 'https://docs.tangly1024.com/article/how-to-question',
 
   // 底部统计面板文案
-  HEO_POST_COUNT_TITLE: '文章数:',
-  HEO_SITE_TIME_TITLE: '建站天数:',
-  HEO_SITE_VISIT_TITLE: '访问量:',  
-  HEO_SITE_VISITOR_TITLE: '访客数:',
+  HEO_POST_COUNT_TITLE: 'Number of articles:',  
+  HEO_SITE_TIME_TITLE: 'Days of station construction:',  
+  HEO_SITE_VISIT_TITLE: 'P-V:',    
+  HEO_SITE_VISITOR_TITLE: 'Number of visitors:',  
 
   // *****  以下配置无效，只是预留开发 ****
   // 菜单配置
