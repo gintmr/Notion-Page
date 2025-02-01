1a1b9f3895b59180d6fd5ded9d7ea62aea6555b3
gintmr <148303202+gintmr@users.noreply.github.com>
2025-02-01 16:58:42 +0800

    Update blog.config.js

--- blog.config.js
+++ blog.config.js
@@ -15,12 +15,12 @@ const BLOG = {
   APPEARANCE_DARK_TIME: process.env.NEXT_PUBLIC_APPEARANCE_DARK_TIME || [18, 6], // 夜间模式起至时间，false时关闭根据时间自动切换夜间模式
 
   AUTHOR: process.env.NEXT_PUBLIC_AUTHOR || 'Gintmr', // 您的昵称 例如 tangly1024  
-  BIO: process.env.NEXT_PUBLIC_BIO || '🍚', // 作者简介  
-  LINK: process.env.NEXT_PUBLIC_LINK || '', // 网站地址    
-  KEYWORDS: process.env.NEXT_PUBLIC_KEYWORD || 'Notion Blog', // 网站关键词 英文逗号隔开    
-  BLOG_FAVICON: process.env.NEXT_PUBLIC_FAVICON || '/favicon.ico', // blog favicon 配置, 默认使用 /public/favicon.ico，支持在线图片，如 https://img.imesong.com/favicon.png  
+  BIO: process.env.NEXT_PUBLIC_BIO || '🥢', // 作者简介    
+  LINK: process.env.NEXT_PUBLIC_LINK || 'https://gintmr.20250130.xyz/', // 网站地址      
+  KEYWORDS: process.env.NEXT_PUBLIC_KEYWORD || "Gintmr's Home", // 网站关键词 英文逗号隔开      
+  BLOG_FAVICON: process.env.NEXT_PUBLIC_FAVICON || '/favicon.ico', // blog favicon 配置, 默认使用 /public/favicon.ico，支持在线图片，如 https://img.imesong.com/favicon.png    
   BEI_AN: process.env.NEXT_PUBLIC_BEI_AN || '', // 备案号 闽ICP备XXXXXX
-  BEI_AN_LINK: process.env.NEXT_PUBLIC_BEI_AN_LINK || 'https://beian.miit.gov.cn/', // 备案查询链接，如果用了萌备等备案请在这里填写
+  BEI_AN_LINK: process.env.NEXT_PUBLIC_BEI_AN_LINK || 'https://beian.miit.gov.cn/', // 备案查询链接，如果用了萌备等备案请在这里填写  
 
   // RSS订阅
   ENABLE_RSS: process.env.NEXT_PUBLIC_ENABLE_RSS || true, // 是否开启RSS订阅功能
