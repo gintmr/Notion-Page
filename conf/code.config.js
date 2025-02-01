0b2daf390e4e9631694a8df86027ebe5d811959e
gintmr <148303202+gintmr@users.noreply.github.com>
2025-02-01 19:42:24 +0800

    Update code.config.js

--- conf/code.config.js
+++ conf/code.config.js
@@ -6,19 +6,19 @@ module.exports = {
   // PrismJs 代码相关
   PRISM_JS_PATH: 'https://npm.elemecdn.com/prismjs@1.29.0/components/',  
   PRISM_JS_AUTO_LOADER:
-    'https://npm.elemecdn.com/prismjs@1.29.0/plugins/autoloader/prism-autoloader.min.js',  
+    'https://npm.elemecdn.com/prismjs@1.29.0/plugins/autoloader/prism-autoloader.min.js',    
 
   // 代码主题 @see https://github.com/PrismJS/prism-themes
   PRISM_THEME_PREFIX_PATH:
     process.env.NEXT_PUBLIC_PRISM_THEME_PREFIX_PATH ||  
-    'https://cdn.jsdelivr.net/npm/prismjs@1.29.0/themes/prism-okaidia.css', // 代码块默认主题  
+    'https://npm.elemecdn.com/prism-themes/themes/prism-dracula.min.css', // 代码块默认主题    
   PRISM_THEME_SWITCH: process.env.NEXT_PUBLIC_PRISM_THEME_SWITCH || true, // 是否开启浅色/深色模式代码主题切换； 开启后将显示以下两个主题
   PRISM_THEME_LIGHT_PATH:
-    process.env.NEXT_PUBLIC_PRISM_THEME_LIGHT_PATH ||  
-    'https://github.com/PrismJS/prism-themes/blob/master/themes/prism-dracula.css', // 浅色模式主题  
+    process.env.NEXT_PUBLIC_PRISM_THEME_LIGHT_PATH ||    
+    'https://npm.elemecdn.com/prism-themes/themes/prism-one-light.css', // 浅色模式主题    
   PRISM_THEME_DARK_PATH:
     process.env.NEXT_PUBLIC_PRISM_THEME_DARK_PATH ||
-    'https://github.com/PrismJS/prism-themes/blob/master/themes/prism-dracula.css', // 深色模式主题  
+    'https://npm.elemecdn.com/prism-themes/themes/prism-dracula.min.css', // 深色模式主题    
 
   CODE_MAC_BAR: process.env.NEXT_PUBLIC_CODE_MAC_BAR || true, // 代码左上角显示mac的红黄绿图标  
   CODE_LINE_NUMBERS: process.env.NEXT_PUBLIC_CODE_LINE_NUMBERS || false, // 是否显示行号
