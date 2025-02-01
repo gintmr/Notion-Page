08dc869a286523abafbf03678dcc6a2168cd2e70
gintmr <w6x6r6@gmail.com>
2025-02-01 20:17:03 +0800

    favicon

--- blog.config.js
+++ blog.config.js
@@ -18,7 +18,7 @@ const BLOG = {
   BIO: process.env.NEXT_PUBLIC_BIO || '🥢', // 作者简介    
   LINK: process.env.NEXT_PUBLIC_LINK || 'https://gintmr.20250130.xyz/', // 网站地址      
   KEYWORDS: process.env.NEXT_PUBLIC_KEYWORD || "Gintmr's Home", // 网站关键词 英文逗号隔开      
-  BLOG_FAVICON: process.env.NEXT_PUBLIC_FAVICON || '/public/gintama1.ico', // blog favicon 配置, 默认使用 /public/favicon.ico，支持在线图片，如 https://img.imesong.com/favicon.png    
+  BLOG_FAVICON: process.env.NEXT_PUBLIC_FAVICON || 'https://ooo.0x0.ooo/2025/02/01/OG4qpx.jpg', // blog favicon 配置, 默认使用 /public/favicon.ico，支持在线图片，如 https://img.imesong.com/favicon.png    
   BEI_AN: process.env.NEXT_PUBLIC_BEI_AN || '', // 备案号 闽ICP备XXXXXX
   BEI_AN_LINK: process.env.NEXT_PUBLIC_BEI_AN_LINK || 'https://beian.miit.gov.cn/', // 备案查询链接，如果用了萌备等备案请在这里填写  
 
